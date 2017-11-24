df173a9180dab4a866a756cdd4001f6118988b4d
Leonardo <bluedraggy@gmail.com>
2017-11-24 16:43:51 +0100

    Develop [Feature] : Adding all basic functionalities.

---
+++ package/tests/main_test.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, =pdate=
+** Project =pname= Testing
+** File description:
+** Unit Testing Project =pname=
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include <ctype.h>
+#include "main.h"
+
+void main_redirect_stdout(void)
+{
+	cr_redirect_stdout();
+}
+
+Test(=pname=, Return_Value_Test)
+{
+	cr_assert_eq("Function to test()", "Int Output..");
+}
+
+Test(=pname=, Stdout_Value_Test, .init = main_redirect_stdout)
+{
+//	Function to test();
+	cr_assert_stdout_eq_str("Str Output..");
+}
